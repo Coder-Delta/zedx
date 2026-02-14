1143c4a66b8cf9aee39e20117f8f813002c6dfc1
Coder-Delta <ranjitbhandary15@gmail.com>
2026-02-14 16:47:52 +0530

    Refactor main.go to include user input functionality and improve imports

--- 03_input/main.go
+++ 03_input/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	welcomeMessage := "Welcome to Go programming!"
 	fmt.Println(welcomeMessage)
+
+	reder := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	//comma ok syntax
+	name, _ := reder.ReadString('\n')//it is actually like try catch in js
+	fmt.Printf("Hello, %s! Nice to meet you.\n", name)
 }
